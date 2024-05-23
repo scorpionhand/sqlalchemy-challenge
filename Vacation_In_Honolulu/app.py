aafb13c60e9a61bed91707ed0c09ba4ef301d6c8
scorpionhand <162941137+scorpionhand@users.noreply.github.com>
2024-05-22 21:44:08 -0500

    Initial Commit

---
+++ Vacation_In_Honolulu/app.py
@@ -0,0 +1,37 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
